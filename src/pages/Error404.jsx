36c87ab5679604a8ccd70b6809afd414dc8e0366
MDJSMD <112764503+MDJSMD@users.noreply.github.com>
2023-01-11 14:15:44 +0100

    Modif et ajout responsive

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,23 +1,30 @@
-import React from "react";
-import Header from "../components/Banner";
-import { Link } from "react-router-dom";
-import Error404 from "../assets/error-404.png";
+import React, { useEffect } from "react"; // Importation de React pour utiliser les composants React
+import { useLocation } from "react-router-dom"; // Importation de useLocation pour avoir les informations de l'emplacement du visiteur
+import { Link } from "react-router-dom"; // Importation de Link pour permettre une navigation vers la page d'acceuil
+import Header from "../components/Header"; // Importation du composant Header depuis le dossier components
 
 const Error = () => {
+  let location = useLocation(); // Utilisation de location pour avoir les informations de l'emplacement du visiteur
+
+  useEffect(() => { 
+    // Utilisation de useEffect pour envoyer un message console d'erreur
+    console.log("La page que vous recherchez n'existe pas", location.pathname);
+  }, [location]);
+
   return (
-    <>
+    <div className="Error404Page">
       <Header />
-      <div className="error-404">
-        <img src={Error404} alt="error-404" />
-        <p className="ErrorPage">
-          Oups ! La page que vous demandez n'existe pas.
-        </p>
-        <Link to="/" className="ErrorLink">
-          Retourner sur la page d’accueil
-        </Link>
+      <h1>404</h1>
+      <p>Désolé, la page que vous avez demandée n'existe pas.</p>  
+
+      {/* Lien pour retourner à la page d'accueil */}
+      <div className="link">  
+      <Link to="/">Retourner à la page d'accueil</Link>
       </div>
-    </>
+    </div>
   );
 };
 
 export default Error;
+
+
