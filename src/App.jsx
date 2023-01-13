b1212fee69ae6c075e691d0151f690b73d3b925b
MDJSMD <112764503+MDJSMD@users.noreply.github.com>
2023-01-13 11:43:40 +0100

    VERSION FINALE

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
+//Importation des modules 
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
-import Banner from './components/Banner'
 import Error from "./pages/Error404";
-import Fiche from "./pages/Fichier";
+import Logements from "./pages/Logements";
+import "./styles/index.css";
 
-
+//Définition du chemin de mes routes
 const App = () => {
     return (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/location/:id" element={<Fiche />} />
+          <Route path="/logement/:id" element={<Logements />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     );
   };
-          
+  
+  //Exportation de mon application
   export default App;
